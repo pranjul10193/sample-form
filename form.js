52f7372e655f9e47a5f895b26edd2d5cc15ecd16
Pranjul <pranjultomar1019@gmail.com>
2015-10-02 00:37:33 +0530

    form set with javascript validation ,respond to multiple errors,differently

--- form.js
+++ form.js
@@ -11,8 +11,12 @@ $(document).ready(function(){
 			return false;
 		}
 	});	
+		var illegalfields=new Array();
+		var errorfields=new Array();
 		function validateForm(){
-			var errorfields=new Array();
+			var error=new Array();
+			var nameillegal=["!","@","#","$","%","^","&","*","(",")","+","=","/","[","]","?","|","<",">","~","`",];
+			var phoneillegal=["!","@","#","$","%","^","&","*","(",")","+","=","/","[","]","?","|","<",">","~","`",];
 			if($('#name').val()==""){
 				errorfields.push('name');
 			}
@@ -28,17 +32,64 @@ $(document).ready(function(){
 			if ($('#password2').val()=="") {
 				errorfields.push('password2');
 			}
-			return errorfields;
+
+			for (var i = 0; i <nameillegal.length; i++) {
+				if ($("#name").val()==nameillegal[i]) {
+					illegalfields.push('name');
+				}
+		    }
+		    error.push("illegalfields");
+			error.push("errorfields");
+			return error;
 		}
 	function provideFeedback(incomingerrors){
-		for(var i=0;i<incomingerrors.length;i++){
-			$('#'+incomingerrors[i]).addClass("errorclass");
-			$('#'+incomingerrors[i]+'error').removeClass("errorfeedback");
+		for (var i = 0; i < incomingerrors.length; i++) {
+			if(incomingerrors[i]=="illegalfields"){
+				for (var j = 0; j < illegalfields.length; j++) {
+					if(illegalfields[j]=="name"){
+						$("#name").addClass('errorclass');
+						$("#nameerror").html("Enter correct name.");
+						$("#nameerror").removeClass("errorfeedback");
+					}
+				}
+			}
+			if(incomingerrors[i]=="errorfields"){
+				for(var j=0; j<errorfields.length;j++){
+					if(errorfields[j]=="name"){
+						$("#name").addClass('errorclass');
+						$("#nameerror").html("Name is required");
+						$("#nameerror").removeClass("errorfeedback");
+					}
+					if (errorfields[j]=="phone") {
+						$("#phone").addClass('errorclass');
+				        $("#phoneerror").removeClass("errorfeedback");
+					}
+					if (errorfields[j]=="email") {
+						$("#email").addClass('errorclass');
+				        $("#emailerror").removeClass("errorfeedback");
+					}
+					if (errorfields[j]=="password1") {
+						$("#password1").addClass('errorclass');
+				        $("#password1error").removeClass("errorfeedback");
+					}
+					if (errorfields[j]=="password2") {
+						$("#password2").addClass('errorclass');
+				        $("#password2error").removeClass("errorfeedback");
+					}
+				}
+			}
 		}
-		$("#errordiv").html("errors encountered");
+
+		//for(var i=0;i<incomingerrors.length;i++){
+			//$('#'+incomingerrors[i]).addClass("errorclass");
+			//$('#'+incomingerrors[i]+'error').removeClass("errorfeedback");
+		//}
+		//$("#errordiv").html("errors encountered");
 	}
 	function removeFeedback(){
-		$('#errordiv').html("");
+		$('#errordiv').html(""); 
+		illegalfields=[];
+		errorfields=[];
 		$("input").each(function(){
 			$(this).removeClass("errorclass");
 		});
