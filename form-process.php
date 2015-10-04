256421b96f3baf4bbe36309b3048310caecddc2f
Pranjul <pranjultomar1019@gmail.com>
2015-10-04 15:56:37 +0530

    php validation on server side for empty fields

--- form-process.php
+++ form-process.php
@@ -1,26 +1,25 @@
 <?php
-if (!isset($_POST['submit'])) {
-	die(header("Location:simpleForm.php"));
+if (!isset($_POST['submit'])) 
+{
+die(header("Location: simpleForm.php"));
 }
 session_start();
-
 $_SESSION['formAttempt']=true;
 if (isset($_SESSION['error'])){
-	unset($_SESSION['error']);
+unset($_SESSION['error']);
 }
-
-$_SESSION['error']=array();
 $required=array("name","email","phone","password1","password2");
-
-foreach ($required as $requiredValue) {
-	if (($_POST[$required]=="") || (!isset($POST[$required]))){
-	$_SESSION['error'][]=$requiredValue." is required";
-	}
+$_SESSION['error']=array();
+foreach ($required as $value) {
+if (!isset($_POST[$value]) || $_POST[$value]=="") {
+$_SESSION['error'][]=$value." is required";
 }
-if((isset($_SESSION['error'])) && count($_SESSION['error']>0)){
-	die(header("Location:simpleForm.php"));
+}
+if(isset($_SESSION['error']) && count($_SESSION['error'])>0){
+die(header("Location: simpleForm.php"));
 }
 else{
-	unset($_SESSION['formAttempt']);
-	die(header("Location:success.php"));
+unset($_SESSION['formAttempt']);
+die(header("Location: success.php"));
 }
+?>
--- simpleForm.php
+++ simpleForm.php
@@ -6,10 +6,10 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=screen-width,initial-scale=1.0" >
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen">
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css" media="screen">
+		 <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css" media="screen">
 		<link rel="stylesheet" type="text/css" href="form.css">
-		<script type="text/javascript" src="bootstrap/js/jquery-1.11.3.min.js"></script>
-		<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+		<!--<script type="text/javascript" src="bootstrap/js/jquery-1.11.3.min.js"></script>-->
+		<!--<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>-->
 		<!--<script type="text/javascript" src="form.js"></script>-->
 		<!--<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
@@ -36,7 +36,9 @@
 		<label for="name">
 			Enter Name*:
 		</label>
-		<input type="text" name="name" id="name"><span class="errorfeedback errorspan" id="nameerror">Name is required</span>
+		<input type="text" name="name" id="name" >
+		<span class="errorfeedback errorspan" id="nameerror">
+		</span>
 		<br>
 		<label for="state">
 			Enter State*:
@@ -59,23 +61,36 @@
 		<input type="text" id="zip" name="zip">
 		<br>
 		<label for="email">Enter E-mail*:</label>
-		<input type="text" id="email" name="email"><span class="errorfeedback errorspan" id="emailerror">Email is Required!</span>
+		<input type="text" id="email" name="email">
+		<span class="errorfeedback errorspan" id="emailerror">	
+		</span>
 		<br>
 		<label for="phone">Enter Phone-No*:</label>
-		<input type="text" id="phone" name="phone"><span class="errorfeedback errorspan" id="phoneerror">Format:xxxx-xxxxxxx</span>
+		<input type="text" id="phone" name="phone" >
+		<span class="errorfeedback errorspan" id="phoneerror">
+		
+		</span>
 		<br>
 		<label for="phonetype">Number Type*:</label>
 			<input class="radioButton" type="radio" name="phonetype" id="work" value="work">
 				<label class="radio-button" for="work">Work</label>
 			<input class="radioButton" type="radio" name="home" id="home" value="home">
 				<label class="radioButton" for="home">Home</label>
-				<span class="errorfeedback errorspan" id="phonetypeerror">please choose an option</span>
+				<span class="errorfeedback errorspan" id="phonetypeerror">
+					
+				</span>
 		<br>
 		<label for="password1">Password*:</label>
-		<input type="password" name="password1" id="password1"><span class="errorfeedback errorspan" id="password1error">Password Required</span>
+		<input type="password" name="password1" id="password1">
+		<span class="errorfeedback errorspan" id="password1error">
+			
+		</span>
 		<br>
 		<label for="password2">Verify Password*:</label>
-		<input type="password" name="password2" id="password2"><span class="errorfeedback errorspan" id="password2error">Password Verification Required</span>
+		<input type="password" name="password2" id="password2">
+		<span class="errorfeedback errorspan" id="password2error">
+			
+		</span>
 		<br>
 		<input type="submit" id="submit" name="submit">			
 	</fieldset>
