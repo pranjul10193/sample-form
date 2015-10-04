87cfecf17efa38ce3c885d96a772390eff30a501
Pranjul <pranjultomar1019@gmail.com>
2015-10-04 16:49:21 +0530

    changed form verification through php

--- form-process.php
+++ form-process.php
@@ -1,18 +1,36 @@
 <?php
-if (!isset($_POST['submit'])) 
+if (!isset($_POST['submit']) && $_SERVER['REQUEST_METHOD']=="POST")
 {
 die(header("Location: simpleForm.php"));
 }
 session_start();
+
 $_SESSION['formAttempt']=true;
 if (isset($_SESSION['error'])){
-unset($_SESSION['error']);
+	unset($_SESSION['error']);
 }
-$required=array("name","email","phone","password1","password2");
-$_SESSION['error']=array();
-foreach ($required as $value) {
-if (!isset($_POST[$value]) || $_POST[$value]=="") {
-$_SESSION['error'][]=$value." is required";
+if (isset($_SESSION['input'])) {
+	unset($_SESSION['input']);
+}
+
+$input=array("name","email","phone","password1","password2");
+$_SESSION['input']=array();
+
+foreach ($input as $value) {
+	$_SESSION['input'][$value]=$_POST[$value];
+}
+
+$required=array("name","email","phone");
+	$_SESSION['error']=array();
+	foreach ($required as $value) {
+	if (!isset($_SESSION['input'][$value]) || $_SESSION['input'][$value]=="") {
+	$_SESSION['error'][$value."Err"]=$value." is required";
+}
+if ($_SESSION['input']['password1']=="") {
+	$_SESSION['error']['password1Err']="Password is required";
+}
+if ($_SESSION['input']['password2']=="") {
+	$_SESSION['error']['password2Err']="Password verification is required";
 }
 }
 if(isset($_SESSION['error']) && count($_SESSION['error'])>0){
--- simpleForm.php
+++ simpleForm.php
@@ -8,8 +8,8 @@
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen">
 		 <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css" media="screen">
 		<link rel="stylesheet" type="text/css" href="form.css">
-		<!--<script type="text/javascript" src="bootstrap/js/jquery-1.11.3.min.js"></script>-->
-		<!--<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>-->
+		<script type="text/javascript" src="bootstrap/js/jquery-1.11.3.min.js"></script>
+		<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 		<!--<script type="text/javascript" src="form.js"></script>-->
 		<!--<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>-->
@@ -27,17 +27,18 @@
 				if (isset($_SESSION['error']) && isset($_SESSION['formAttempt'])){
 					unset($_SESSION['formAttempt']);
 					print("Errors Encountered<br>\n");
-					foreach($_SESSION['error'] as $error){
-						print($error."<br>\n");
-					}
+					//foreach($_SESSION['error'] as $error){
+					//	print($error."<br>\n");
+					//}
 				}
 			?>
 		</div>
 		<label for="name">
 			Enter Name*:
 		</label>
-		<input type="text" name="name" id="name" >
+		<input type="text" name="name" id="name" value="<?php print(@$_SESSION['input']['name']); ?>">
 		<span class="errorfeedback errorspan" id="nameerror">
+			<?php print(@$_SESSION['error']['nameErr']); unset($_SESSION['error']['nameErr']);?>
 		</span>
 		<br>
 		<label for="state">
@@ -61,14 +62,18 @@
 		<input type="text" id="zip" name="zip">
 		<br>
 		<label for="email">Enter E-mail*:</label>
-		<input type="text" id="email" name="email">
-		<span class="errorfeedback errorspan" id="emailerror">	
+		<input type="text" id="email" name="email" value="<?php print(@$_SESSION['input']['email']); ?>">
+		<span class="errorfeedback errorspan" id="emailerror">
+			<?php print(@$_SESSION['error']['emailErr']);
+					//unset($_SESSION['error']['emailErr']);
+			 ?>	
 		</span>
 		<br>
 		<label for="phone">Enter Phone-No*:</label>
-		<input type="text" id="phone" name="phone" >
+		<input type="text" id="phone" name="phone" value="<?php print(@$_SESSION['input']['phone']); ?>">
 		<span class="errorfeedback errorspan" id="phoneerror">
-		
+			<?php print(@$_SESSION['error']['phoneErr']); 
+			?>
 		</span>
 		<br>
 		<label for="phonetype">Number Type*:</label>
@@ -77,19 +82,21 @@
 			<input class="radioButton" type="radio" name="home" id="home" value="home">
 				<label class="radioButton" for="home">Home</label>
 				<span class="errorfeedback errorspan" id="phonetypeerror">
-					
+					<?php print(@$_SESSION['error']['phonetypeErr']);?>
 				</span>
 		<br>
 		<label for="password1">Password*:</label>
 		<input type="password" name="password1" id="password1">
 		<span class="errorfeedback errorspan" id="password1error">
-			
+			<?php print(@$_SESSION['error']['password1Err']);
+			 ?>
 		</span>
 		<br>
 		<label for="password2">Verify Password*:</label>
 		<input type="password" name="password2" id="password2">
 		<span class="errorfeedback errorspan" id="password2error">
-			
+			<?php print(@$_SESSION['error']['password2Err']); 
+			?>
 		</span>
 		<br>
 		<input type="submit" id="submit" name="submit">			
